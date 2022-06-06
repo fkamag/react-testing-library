99c339eaf44d9b0798007d4f52706f852794e067
fkamag <fabiokamagian@gmail.com>
2022-06-06 16:33:05 -0300

    requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import App from '../App';
+
+describe('Testa se o topo da aplicação contém um conjunto de links de navegação', () => {
+  test('Testa o componente App.js', () => {
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>,
+    );
+
+    const homeEl = screen.getByRole('link', { name: /home/i });
+    expect(homeEl).toBeInTheDocument();
+
+    const aboutEl = screen.getByRole('link', { name: /about/i });
+    expect(aboutEl).toBeInTheDocument();
+
+    const favoriteEl = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(favoriteEl).toBeInTheDocument();
+  });
+});
