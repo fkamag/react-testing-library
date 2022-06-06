5519ebcad795a758e7ae09445cb18c0a0235736e
fkamag <fabiokamagian@gmail.com>
2022-06-06 17:29:15 -0300

    requisito 2

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,3 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import About from '../pages/About';
 
-// iniciando
+test('Testa componente About', () => {
+  render(
+    <BrowserRouter>
+      <About />
+    </BrowserRouter>,
+  );
+
+  const headingEl = screen.getByRole('heading', { name: /About Pokédex/i, level: 2 });
+  expect(headingEl).toBeInTheDocument();
+
+  const paragraphs = screen.getAllByText(/Pokémons/i);
+  expect(paragraphs.length).toBe(2);
+
+  const imgEl = screen.getByRole('img', { name: /Pokédex/i });
+  const urlEl = 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png';
+  expect(imgEl).toBeInTheDocument();
+  expect(imgEl.src).toBe(urlEl);
+});
