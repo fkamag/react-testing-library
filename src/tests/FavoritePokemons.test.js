db1eaa4a8a95b39b8bfd5589079c8d334cfd4879
fkamag <fabiokamagian@gmail.com>
2022-06-06 20:23:41 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,15 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import FavoritePokemons from '../pages/FavoritePokemons';
+
+test('Testa o componente Favorite Pokémons', () => {
+  render(
+    <BrowserRouter>
+      <FavoritePokemons />
+    </BrowserRouter>,
+  );
+
+  const noPokemons = screen.getByText('No favorite pokemon found');
+  expect(noPokemons).toBeInTheDocument();
+});
